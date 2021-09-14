032a567598f730f543cc0ea7b2ff4b0a9c877585
skeshavaa <s.keshavaa@gmail.com>
2021-09-13 23:25:47 -0400

    added see more button

--- src/pages/installations.tsx
+++ src/pages/installations.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { graphql, Link as GatsbyLink, useStaticQuery } from 'gatsby';
 import { GatsbyImage } from 'gatsby-plugin-image';
 import {
@@ -10,7 +10,8 @@ import {
   Input,
   InputGroup,
   InputLeftElement,
-  Divider
+  Divider,
+  Button
 } from '@chakra-ui/react';
 
 import { Layout, Seo } from '../components';
@@ -39,7 +40,27 @@ const InstallationPage = () => {
       }
     }
   `);
-  console.log(data);
+  console.log(data.installations.nodes.length);
+
+  const entryOne = Math.floor(Math.random() * data.installations.nodes.length)
+  const entryTwo = Math.floor(Math.random() * data.installations.nodes.length)
+  const entryThree = Math.floor(Math.random() * data.installations.nodes.length)
+  const entryFour = Math.floor(Math.random() * data.installations.nodes.length)
+
+  const preview = [data.installations.nodes[entryOne], data.installations.nodes[entryTwo],
+  data.installations.nodes[entryThree], data.installations.nodes[entryFour]]
+
+  const [isPreview, setIsPreview] = useState(true)
+
+  console.log(data)
+  console.log(data.installations.nodes[entryOne])
+
+  // data.installations.nodes.splice(entryOne, 1)
+  // data.installations.nodes.splice(entryTwo, 1)
+  // data.installations.nodes.splice(entryThree, 1)
+  // data.installations.nodes.splice(entryFour, 1)
+
+
   return (
     <Layout>
       <Seo title="Installations" />
@@ -100,7 +121,7 @@ const InstallationPage = () => {
         Click on an installation to learn more!
       </Heading> */}
       <SimpleGrid columns={[2, null, 4]} spacing={2}>
-        {data.installations.nodes.map((data: any, i: number) => (
+        {isPreview ? preview.map((data: any, i: number) => (
           <Center>
             <Link
               as={GatsbyLink}
@@ -139,8 +160,55 @@ const InstallationPage = () => {
               ))}
             </Link>
           </Center>
-        ))}
+        ))
+          :
+          data.installations.nodes.map((data: any, i: number) => (
+            <Center>
+              <Link
+                as={GatsbyLink}
+                to={`/installation/${CreateFriendlyUrl(
+                  data.title,
+                  data.remoteId
+                )}`}
+              >
+                <GatsbyImage
+                  objectFit="cover"
+                  image={
+                    data.images[0] && data.images[0].localFile.childImageSharp
+                      ? data.images[0].localFile.childImageSharp.gatsbyImageData
+                      : ''
+                  }
+                  alt={data.images[0] ? data.images[0].altText : data.title}
+                />
+                <Heading
+                  as="h3"
+                  size="md"
+                  color="primary.800"
+                  fontWeight="bold"
+                  lineHeight={1.5}
+                  textAlign={['center', 'center', 'left', 'left']}
+                >
+                  {data.title}
+                </Heading>
+                {data.profiles.map((data: any, i: number) => (
+                  <Text
+                    size="md"
+                    color="primary.800"
+                    textAlign={['center', 'center', 'left', 'left']}
+                  >
+                    {data.name}
+                  </Text>
+                ))}
+              </Link>
+            </Center>
+          ))}
+
       </SimpleGrid>
+      <Button width="200px" marginTop="20px" onClick={() => {
+        setIsPreview(!isPreview)
+      }}>
+        {isPreview ? "See More" : "See Less"}
+      </Button>
     </Layout>
   );
 };
